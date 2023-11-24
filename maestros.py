8eb517a34060e9e0d05a4004c16f2094adee9a47
Emili2122 <Emili2122>
2023-11-24 11:33:34 -0600

    Se añadio un nuevo maestro al codigo maestros

--- maestros.py
+++ maestros.py
@@ -2,6 +2,7 @@ maestros = [
     {'name': 'Gabriel', 'Materia': 'Automatizacion de Infrestructura digital', 'Numero de empleado': 2001},
     {'name': 'Patricia', 'Materia': 'Optativa 1', 'Numero de empleado': 2002},
     {'name': 'Alan', 'Materia': 'sistema de calidad para TI', 'Numero de empleado': 2003},
+    {'name': 'Roberto', 'Materia': 'Hacking Etico', 'Numero de empleado': 2004},  
 
 
 ]
